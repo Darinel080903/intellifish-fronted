5cd6f380526ecdaa559203d36145aa8234d14be3
Darinel Monjaras Ruiz <darinelmr12345@gmail.com>
2023-07-16 13:15:23 -0600

    API conection with add fish

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosAPIInstance = axios.create({
-    baseURL: '127.0.0.1:8080'
+    baseURL: 'http://localhost:8080'
 });
-
+    
 export default axios;
--- src/pages/AddFish.jsx
+++ src/pages/AddFish.jsx
@@ -2,8 +2,30 @@ import Navbar from "../containers/Navbar/Navbar"
 import Input from "../components/Input/Input"
 import Button from "../components/Button/Button"
 import AddFishImage from "../assets/img/AddFish-image.jpg"
+import React, { useState } from 'react';
+import { axiosAPIInstance } from "../api/axios";
 
 function AddFish() {
+    const [NameFish, setNameFish] = useState('');
+    const [SpicesFish, setSpicesFish] = useState('');
+    const [urlImagen, setUrlImagen] = useState('');
+
+    const handleAddFish = () => {
+        const data = {
+            nombre: NameFish,
+            especie: SpicesFish,
+            imagen: urlImagen
+        };
+        axiosAPIInstance.post(data)
+            .then((response) => {
+                console.log('Respuesta del servidor:', response.data);
+            
+        })
+        .catch((error) => {
+            console.error('Error al enviar la solicitud:', error);
+            // Manejar errores aquí si es necesario
+        });
+    }
     return(
         <>
             <Navbar/>
@@ -18,21 +40,33 @@ function AddFish() {
 
                         <div className='flex flex-col py-2'>
                             <label>Nombre del Pez</label>
-                            <Input/>
+                            <Input
+                                type='text'
+                                value={NameFish}
+                                onChange={(e) => setNameFish(e.target.value)}
+                            />
                         </div>
 
                         <div className='flex flex-col py-2'>
                             <label>Especie del Pez</label>
-                            <Input/>
+                            <Input
+                                type='text'
+                                value={SpicesFish}
+                                onChange={(e) => setSpicesFish(e.target.value)}
+                            />
                         </div>
 
                         <div className='flex flex-col py-2'>
                             <label>Url de la imagen</label>
-                            <Input/>
+                            <Input
+                                type='text'
+                                value={urlImagen}
+                                onChange={(e) => setUrlImagen(e.target.value)}
+                            />
                         </div>
 
                         <div className='flex flex-col py-2'>
-                            <Button>Agregar Pez</Button>
+                            <Button onClick={handleAddFish}>Agregar Pez</Button>
                         </div>            
                     </form>
                 </div>
